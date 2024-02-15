02d4d05a3934940baee37b1f227bf8ae9fd0f9dc
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2024-02-15 08:51:42 +0200

    category active link fix

--- HardwareStore.App/Services/Data/Products/CreationStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HardwareStore.App.Services.Data.Products
-{
-    public class CreationStatus
-    {
-        public int? Id { get; set; } = null;
-        public bool IsSucssessfull { get; set; } = true;
-        public List<string> Messages { get; set; } = new List<string>();
-    }
-}
