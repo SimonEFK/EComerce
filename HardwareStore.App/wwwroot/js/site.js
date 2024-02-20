4e40138c50f9755de7df55853099ffa254054700
SimonEFK <79474513+SimonEFK@users.noreply.github.com>
2024-02-20 17:12:18 +0200

    adding items to cart js refactoring

--- HardwareStore.App/wwwroot/js/site.js
+++ HardwareStore.App/wwwroot/js/site.js
@@ -1,1 +1,27 @@
-﻿
+﻿async function AddProductToCart(event) {
+    let target = event.target;
+    if (target.matches("[data-addProductToCart]")) {
+
+        let Id = Number(target.dataset.productid);
+
+        const formData = new FormData();
+        formData.append('productId', Id);
+
+        let url = "/Cart/AddItemToCart";
+
+        let response = await fetch(url, {
+            method: "POST",
+            body: formData
+        });
+
+        if (response.ok) {
+            
+            let productName = target.dataset.addproducttocart;
+            let toast = new bootstrap.Toast('.toast');
+            let toastMessageSpan = document.querySelector('.toast-message');
+            toastMessageSpan.textContent = `${productName} added to cart`;
+            toast.show();
+            let parser = new DOMParser();
+        }
+    }
+}
