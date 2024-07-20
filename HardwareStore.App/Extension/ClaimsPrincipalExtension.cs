92cd5090bf87c88b5156d6c5cc3a35731d27071e
Simon E. Funu-Kraka <simonfunu-kraka@outlook.com>
2024-07-20 15:27:47 +0300

    extending claimsPrincipal to access userId easier

---
+++ HardwareStore.App/Extension/ClaimsPrincipalExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace HardwareStore.App.Extension
+{
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using System.Security.Claims;
+
+    public static class ClaimsPrincipalExtension
+    {
+        public static string? Id(this ClaimsPrincipal claimsPrincipal)
+        {
+            var userId = claimsPrincipal.Claims
+                .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)
+                ?.Value;
+
+            return userId;
+        }
+    }
+}
